41f07e957df455881cdff1b6a30a8e8f4675222c
JayWilson <heyjay@cctplus.dev>
2022-11-18 14:39:50 -0600

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Maegan Wilson on 11/18/22.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
